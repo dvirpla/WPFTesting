69277f10e88db35ced523844e968f5547564288b
Kfir <kfirprods@gmail.com>
2020-03-17 16:52:33 +0200

    Basic todo app for future testing

---
+++ TodoWpfApp/TodoWpfApp/ViewModels/ControllerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace TodoWpfApp.ViewModels
+{
+    public class ControllerViewModel
+    {
+        public ObservableCollection<TodoItemViewModel> TodoItems { get; }
+
+        public ControllerViewModel()
+        {
+            this.TodoItems = new ObservableCollection<TodoItemViewModel>();
+
+            // For test purposes:
+            this.TodoItems.Add(
+                new TodoItemViewModel("Test", DateTime.Now.AddDays(1))
+            );
+        }
+    }
+}
---
+++ TodoWpfApp/TodoWpfApp/ViewModels/TodoItemViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TodoWpfApp.Resources;
+
+namespace TodoWpfApp.ViewModels
+{
+    public class TodoItemViewModel : INotifyPropertyChanged
+    {
+        public string Description { get; set; }
+
+        public DateTime DueBy { get; set; }
+
+        private bool _isDone;
+
+        public bool IsDone
+        {
+            get => this._isDone;
+
+            set
+            {
+                this._isDone = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ICommand MarkAsDoneCommand => new DelegateCommand(_ => { this.IsDone = true; });
+
+        public TodoItemViewModel(string description, DateTime dueBy)
+        {
+            this.Description = description;
+            this.DueBy = dueBy;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
