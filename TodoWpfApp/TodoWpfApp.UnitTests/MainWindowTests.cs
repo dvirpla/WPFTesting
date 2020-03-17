9370b0428623af54745861312c4ac9cca4f73520
dvirpla <dvirpla@gmail.com>
2020-03-17 17:50:02 +0200

    Added tests

---
+++ TodoWpfApp/TodoWpfApp.UnitTests/MainWindowTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlaUI.UIA3;
+
+   
+
+namespace TodoWpfApp.UnitTests
+{
+    [TestClass]
+    public class MainWindowTests
+    {
+        [TestMethod]
+        public void Title_CheckTitle_CheckEqualToTodoWpfApp()
+        {
+            var app = FlaUI.Core.Application.Launch(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TodoWpfApp.exe"));
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                Assert.AreEqual(window.Title, "TodoWpfApp");
+            }
+            app.Close();
+        }
+
+        [TestMethod]
+        public void Title_CheckItemsControlExists_ItemsControlExists()
+        {
+            var app = FlaUI.Core.Application.Launch(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TodoWpfApp.exe"));
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                var cf = new ConditionFactory(new UIA3PropertyLibrary());
+                var todoItem = window.FindFirstDescendant(cf.ByName("TodoItemsControl"));
+                Assert.IsNotNull(todoItem);
+            }
+            app.Close();
+        }
+    }
+}
---
+++ TodoWpfApp/TodoWpfApp.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TodoWpfApp.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TodoWpfApp.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6824929a-d5ae-4651-887d-fcc2f11764c2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
