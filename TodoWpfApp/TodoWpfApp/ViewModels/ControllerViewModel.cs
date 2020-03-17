c0f5a9811f9257e1fe6b88a67f3c4f3a1e5cc1a4
Kfir <kfirprods@gmail.com>
2020-03-17 17:37:58 +0200

    Items can now be added

--- TodoWpfApp/TodoWpfApp/ViewModels/ControllerViewModel.cs
+++ TodoWpfApp/TodoWpfApp/ViewModels/ControllerViewModel.cs
@@ -1,20 +1,66 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using TodoWpfApp.Resources;
 
 namespace TodoWpfApp.ViewModels
 {
-    public class ControllerViewModel
+    public class ControllerViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<TodoItemViewModel> TodoItems { get; }
 
+        private string _newDescription;
+
+        public string NewDescription
+        {
+            get => _newDescription;
+            set
+            {
+                _newDescription = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private DateTime _newDate;
+
+        public DateTime NewDate
+        {
+            get => this._newDate;
+            set
+            {
+                this._newDate = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddNewTodoItemCommand => new DelegateCommand(_ =>
+        {
+            this.TodoItems.Add(new TodoItemViewModel(this.NewDescription, this.NewDate));
+
+            this.NewDescription = "";
+            this.NewDate = DateTime.Now;
+        });
+
         public ControllerViewModel()
         {
+            this.NewDescription = "";
+            this.NewDate = DateTime.Now;
+
             this.TodoItems = new ObservableCollection<TodoItemViewModel>();
 
             // For test purposes:
             this.TodoItems.Add(
                 new TodoItemViewModel("Test", DateTime.Now.AddDays(1))
             );
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
