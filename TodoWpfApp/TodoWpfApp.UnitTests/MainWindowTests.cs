26cc3ee94336235261f88ed6e6cfb6f0ab821758
dvirpla <dvirpla@gmail.com>
2020-03-18 11:37:55 +0200

    Added NewTodoItemExists test

--- TodoWpfApp/TodoWpfApp.UnitTests/MainWindowTests.cs
+++ TodoWpfApp/TodoWpfApp.UnitTests/MainWindowTests.cs
@@ -25,14 +25,28 @@ namespace TodoWpfApp.UnitTests
         }
 
         [TestMethod]
-        public void Title_CheckItemsControlExists_ItemsControlExists()
+        public void TodoItemView_CheckTodoItemViewExists_TodoItemViewExists()
         {
             var app = FlaUI.Core.Application.Launch(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TodoWpfApp.exe"));
             using (var automation = new UIA3Automation())
             {
                 var window = app.GetMainWindow(automation);
                 var cf = new ConditionFactory(new UIA3PropertyLibrary());
-                var todoItem = window.FindFirstDescendant(cf.ByName("TodoItemsControl"));
+                var todoItem = window.FindFirstChild(cf.ByClassName("TodoItemView"));
+                Assert.IsNotNull(todoItem);
+            }
+            app.Close();
+        }
+
+        [TestMethod]
+        public void NewTodoItemView_CheckNewTodoItemViewExists_NewTodoItemViewExists()
+        {
+            var app = FlaUI.Core.Application.Launch(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TodoWpfApp.exe"));
+            using (var automation = new UIA3Automation())
+            {
+                var window = app.GetMainWindow(automation);
+                var cf = new ConditionFactory(new UIA3PropertyLibrary());
+                var todoItem = window.FindFirstChild(cf.ByClassName("NewTodoItemView"));
                 Assert.IsNotNull(todoItem);
             }
             app.Close();
